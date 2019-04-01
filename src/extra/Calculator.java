33ce07f63ea238077be07e2259b58b518d0e7ee1
ArielBaruch <league@WTS14-4.attlocal.net>
2019-04-01 14:28:05 -0700

    calculator ip

--- src/extra/Calculator.java
+++ src/extra/Calculator.java
@@ -1,17 +1,58 @@
 package extra;
 
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class Calculator {
 
-public static double add(){
-	return 0;
+ double add(double num1,double num2){
+	double answer= num1+num2;
+	
+	return answer;
 }
-public static double subtract() {
-	return 0;
+ double subtract(double num1,double num2) {
+	double answer2= num1-num2;
+	 
+	 return answer2;
 }
-public static double multiply() {
-	return 0;
+ double multiply(double num1,double num2) {
+	double answer3= num1*num2;
+	 
+	 return answer3;
 }
-public static double divide() {
-	return 0;
+ double divide(double num1,double num2) {
+	
+	 double answer4= num1/num2;
+	 return answer4;
 }
+ public void view() {
+	JFrame frame=new JFrame();
+	JButton button= new JButton();
+	JButton button2= new JButton();
+	JButton button3= new JButton();
+	JButton button4= new JButton();
+	JTextField field=new JTextField(15);
+	JTextField field2=new JTextField(15);
+	JPanel panel = new JPanel();
+	frame.setVisible(true);
+	button.setText("add");
+	button2.setText("sub");
+	button3.setText("mult");
+	button4.setText("div");
+	panel.add(field);
+	panel.add(field2);
+
+	panel.add(button);
+	panel.add(button2);
+	panel.add(button3);
+	panel.add(button4);
+	//panel.add(field);
+	//panel.add(field);
+	frame.add(panel);
+	frame.pack();
+ }
 }
