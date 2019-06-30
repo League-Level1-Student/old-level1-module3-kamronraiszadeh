cc64dcdf099b14b7e2f75800634c86e71a61c874
League Student <league@WTS14-4.attlocal.net>
2019-06-30 12:55:44 -0700

    typing tutor
    :100:

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener {
@@ -19,18 +21,19 @@ public class TypingTutor implements KeyListener {
 	public void view() {
 
 		frame.setVisible(true);
-		JPanel panel = new JPanel();
+
 		frame.setTitle("TYPE NOW");
 		frame.setSize(350, 350);
-		frame.add(panel);
+
+		frame.add(label);
 		frame.setDefaultCloseOperation(3);
 		currentLetter = generateRandomLetter();
-
-		label.setText(currentLetter + "");
-
 		label.setFont(label.getFont().deriveFont(28.0f));
 		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setText(currentLetter + "");
+
 		frame.addKeyListener(this);
+		frame.pack();
 	}
 
 	JFrame frame = new JFrame();
@@ -40,28 +43,41 @@ public class TypingTutor implements KeyListener {
 		Random r = new Random();
 		return (char) (r.nextInt(26) + 'a');
 	}
+	
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 
-		label.setText(currentLetter + "");
+		// label.setText(currentLetter + "");
 	}
+	Date timeAtStart = new Date();
 
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	      Date timeAtEnd = new Date();
+	      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	      long gameInSeconds = (gameDuration / 1000) % 60;
+	      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	      int charactersPerMinute = (int) (charactersPerSecond * 60);
+	      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 
-		System.out.println("you typed " +currentLetter+ "");
-
+		System.out.println("you typed " + e.getKeyChar() + "");
+		System.out.println("current letter is " + currentLetter);
 		if (currentLetter == e.getKeyChar()) {
 			frame.setBackground(Color.GREEN);
+			label.setBackground(Color.GREEN);
+			System.out.println("correct");
 		} else {
 			frame.setBackground(Color.RED);
+
 		}
+		frame.pack();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
+		
 	}
 }
