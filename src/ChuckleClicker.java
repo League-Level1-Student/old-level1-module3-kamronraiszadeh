c8bbbf43831fa4f2987091968294460798592d8f
League Student <league@iMac-5.attlocal.net>
2019-08-19 16:23:05 -0700

    :100:

--- src/ChuckleClicker.java
+++ src/ChuckleClicker.java
@@ -6,35 +6,39 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker implements ActionListener {
-
+public class ChuckleClicker implements ActionListener  {
+JFrame frame = new JFrame();
+JPanel panel= new JPanel();
+JButton button =new JButton();
+JButton button2 = new JButton();
 public static void main(String[] args) {
 	ChuckleClicker cc = new ChuckleClicker();
 	cc.makeButtons();
 }
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
-JButton button = new JButton();
-JButton button2 = new JButton();
 public void makeButtons() {
-frame.setVisible(true);
-button.setText("joke");
-button2.setText("punchline");
-button.addActionListener(this);
-button2.addActionListener(this);
-panel.add(button);
-panel.add(button2);
-frame.add(panel);
-frame.pack();
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	button.setText("joke");
+	button2.setText("punchline");
+	button.addActionListener(this);
+	button2.addActionListener(this);
+	panel.add(button);
+	panel.add(button2);
+	frame.add(panel);
+	frame.pack();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 }
 @Override
 public void actionPerformed(ActionEvent e) {
-	if (button==e.getSource()) 
-		JOptionPane.showMessageDialog(null, "What do you call a overweight psychic?");
-		 if (button2==e.getSource()) 
-		JOptionPane.showMessageDialog(null, "A four-chin teller");
+	if (e.getSource()==button) {
+		JOptionPane.showMessageDialog(null,"what do you call an overweight psychic");
 	}
+		if (e.getSource()==button2) {
+			JOptionPane.showMessageDialog(null, "a four-chin teller ");
+			
+		
+	}
+}
 		
 	
 	
